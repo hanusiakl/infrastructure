af8ed9570900cfecfd10ff6385fd7bf0e13f8e63
lhanusiak <lhanusiak@gmail.com>
2021-09-12 20:16:03 +0200

    - add multi environment

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # infrastructure
+
+`kubectl --namespace jenkins port-forward svc/jenkins 8080:8080` - forward to jenkins
+
+
+https://github.com/jenkinsci/helm-charts/blob/main/charts/jenkins/values.yaml
---
+++ recreate.bat
@@ -0,0 +1,12 @@
+@echo off
+echo "Deleting jenkins..."
+helm delete jenkins -n=jenkins
+
+REM Deleting jenkins deployment can take a time especially deleting pvc
+REM To be sure that pvc is deleted run `kubectl get pvc --namespace=jenkins`
+timeout /t 15 /nobreak
+
+echo "Installing jenkins..."
+helm install jenkins jenkins/jenkins  -f ./helm/values.yaml -n=jenkins
+
+
