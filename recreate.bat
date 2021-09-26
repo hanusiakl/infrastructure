f40a482740821b42ce5182d952d1f5a70e923a39
Leszek Hanusiak <lhanusiak@gmail.com>
2021-09-26 22:16:10 +0200

    update

--- recreate.bat
+++ recreate.bat
@@ -4,7 +4,7 @@ helm delete jenkins -n=jenkins
 
 REM Deleting jenkins deployment can take a time especially deleting pvc
 REM To be sure that pvc is deleted run `kubectl get pvc --namespace=jenkins`
-timeout /t 15 /nobreak
+timeout /t 20 /nobreak
 
 echo "Installing jenkins..."
 helm install jenkins jenkins/jenkins  -f ./helm/jenkins/values.yaml -n=jenkins
